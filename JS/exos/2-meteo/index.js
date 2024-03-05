1e12f8813296efefadf59b85442acda6fef5e8d6
SohamHUG <153825747+SohamHUG@users.noreply.github.com>
2024-03-05 14:33:14 +0400

    Update index.js

--- JS/exos/2-meteo/index.js
+++ JS/exos/2-meteo/index.js
@@ -1,13 +1,13 @@
+const apiKey = '1587159bb0692bc32c752afb5ebd8948';
 async function weather() {
-    const apiKey = '1587159bb0692bc32c752afb5ebd8948'; 
     const city = document.getElementById('cityInput').value.trim();
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     const weatherInfo = document.getElementById('weatherInfo');
-    
+
     try {
         const response = await axios.get(url);
         const data = response.data;
-        
+
         const descriptionMeteo = data.weather[0].description;
         const temperature = data.main.temp;
         const humidite = data.main.humidity;
@@ -20,6 +20,30 @@ async function weather() {
     } catch (error) {
         weatherInfo.innerHTML = `<p>${error.message}</p>`;
     }
+}
+
+function getLocation() {
+    navigator.geolocation.getCurrentPosition(async (position) => {
+        const latitude = position.coords.latitude;
+        const longitude = position.coords.longitude;
+        try {
+            const response = await axios.get(`https://api.openweathermap.org/geo/1.0/reverse?lat=${latitude}&lon=${longitude}&appid=${apiKey}`);
+            const data = await response.data;
+            const cityName = data[0].name;
+
+            const response2 = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`);
+            const data2 = await response2.data;
+            const temperature = data2.main.temp;
+
+            document.getElementById('coordinates').innerHTML = `
+            <p>Latitude: ${latitude}, Longitude: ${longitude}</p>
+            <p>Ville: ${cityName}</p>
+            <p>Température : ${temperature} °C</p>
+            `;
+        } catch (error) {
+            document.getElementById('coordinates').innerHTML = `Erreur: ${error.message}`;
+        }
+    });
 }
 
 
